4c8f5099ad8ddcbf6df43d07466e5af93e6bc22d
Shivam <thisisshivam06@gmail.com>
2022-09-22 20:28:30 +0530

    Taking Input

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,12 @@
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
+	
+	
+	public static void main(String args[]){
+// if you use println java will put a new line
 		System.out.println("HelloWorld");
 		System.out.print("HelloWorld");
 		System.out.println("HelloWorld");
-
 	}
-
 }
