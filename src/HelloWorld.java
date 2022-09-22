7d4f6ce7648227ef12a580a6d2000c8a96819ca9
Shivam <thisisshivam06@gmail.com>
2022-09-22 18:34:56 +0530

    first Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+		System.out.print("HelloWorld");
+		System.out.println("HelloWorld");
+
+	}
+
+}
